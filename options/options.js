dc61d8bfc367c7549003ab35f8f2f9d52351de9d
Crowdbotics <team@crowdbotics.com>
2023-08-21 08:43:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "still-butterfly-42989",
+  url: "https://still-butterfly-42989.botics.co",
+  api: "https://still-butterfly-42989.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
